b2fcb957952a35f9865d14128bd9729196d49d54
muhdmirzamz <muhdmirzamz@gmail.com>
2014-01-13 22:04:49 +0800

    Initial commit

---
+++ Calculator/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Calculator
+//
+//  Created by Muhd Mirza on 13/1/14.
+//  Copyright (c) 2014 Muhd Mirza. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
