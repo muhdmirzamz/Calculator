8bddaecfdf392213850ac7e0e98a4fbe719d29d1
muhdmirzamz <muhdmirzamz@gmail.com>
2014-01-14 13:04:08 +0800

    Added main menu

---
+++ Calculator/Validation/include/Validation.h
@@ -0,0 +1,14 @@
+//
+//  Validation.h
+//  Calculator
+//
+//  Created by Muhd Mirza on 14/1/14.
+//  Copyright (c) 2014 Muhd Mirza. All rights reserved.
+//
+
+#ifndef __Calculator__Validation__
+#define __Calculator__Validation__
+
+#include <iostream>
+
+#endif /* defined(__Calculator__Validation__) */
---
+++ Calculator/Validation/src/Validation.cpp
@@ -0,0 +1,9 @@
+//
+//  Validation.cpp
+//  Calculator
+//
+//  Created by Muhd Mirza on 14/1/14.
+//  Copyright (c) 2014 Muhd Mirza. All rights reserved.
+//
+
+#include "Validation.h"
