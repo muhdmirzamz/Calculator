06e306fc21c1c9136e15ee5fe9b69d2c44feaf39
muhdmirzamz <muhdmirzamz@gmail.com>
2014-01-30 18:55:22 +0800

    ReStructured project

---
+++ osx/Calculator/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  Calculator
+//	Program starts here
+//
+//  Created by Muhd Mirza on 13/1/14.
+//  Copyright (c) 2014 Muhd Mirza. All rights reserved.
+//
+
+#include <iostream>
+
+#include "MainMenu.h"
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+	MainMenu *mainMenu = new MainMenu();
+	mainMenu->showOptionsScreen();
+	
+	delete mainMenu;
+    return 0;
+}
+
