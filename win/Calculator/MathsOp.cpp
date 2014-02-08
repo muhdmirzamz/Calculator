6f822c132519e27b5b0855ee200650a084742111
muhdmirzamzdev <muhdmirzamzdev@gmail.com>
2014-02-08 18:45:11 +0800

    First windows release

--- win/Calculator/MathsOp.cpp
+++ win/Calculator/MathsOp.cpp
@@ -32,17 +32,116 @@ void MathsOp::add() {
         add();
     }
 
+    sum = addDoubleOne + addDoubleTwo;
+    cout << "Sum: " << sum << endl;
 
+    endScreen();
 }
 
 void MathsOp::subtract() {
+    showTitleOfMathsOp("SUBTRACT");
 
+    cout << "Enter first number: ";
+    cin >> subtractDoubleOne;
+    if (cin.fail()) {
+        showErrorMessage();
+        subtract();
+    }
+
+    cout << "Enter second number: ";
+    cin >> subtractDoubleTwo;
+    if (cin.fail()) {
+        showErrorMessage();
+        subtract();
+    }
+
+    difference = subtractDoubleOne - subtractDoubleTwo;
+    cout << "Difference: " << difference << endl;
+
+    endScreen();
 }
 
 void MathsOp::multiply() {
+    showTitleOfMathsOp("MULTIPLY");
 
+    cout << "Enter first number: ";
+    cin >> multiplyDoubleOne;
+    if (cin.fail()) {
+        showErrorMessage();
+        multiply();
+    }
+
+    cout << "Enter second number: ";
+    cin >> multiplyDoubleTwo;
+    if (cin.fail()) {
+        showErrorMessage();
+        multiply();
+    }
+
+    product = multiplyDoubleOne * multiplyDoubleTwo;
+    cout << "Product: " << product << endl;
+
+    endScreen();
 }
 
 void MathsOp::divide() {
+    showTitleOfMathsOp("DIVIDE");
 
+    cout << "Enter first number: ";
+    cin >> divideDoubleOne;
+    if (cin.fail()) {
+        showErrorMessage();
+        divide();
+    }
+
+    cout << "Enter second number: ";
+    cin >> divideDoubleTwo;
+    if (cin.fail()) {
+        showErrorMessage();
+        divide();
+    }
+
+    division = divideDoubleOne / divideDoubleTwo;
+    cout << "Division: " << division << endl;
+
+    endScreen();
+}
+
+void MathsOp::endScreen() {
+    cout << "What do you want to do?" << endl;
+
+    cout << "1 - ADD" << endl;
+    cout << "2 - SUBTRACT" << endl;
+    cout << "3 - MULTIPLY" << endl;
+    cout << "4 - DIVIDE" << endl;
+    cout << "5 - END" << endl;
+
+    cout << "Your choice: ";
+    cin >> endScreenChoice;
+    if (cin.fail()) {
+        cin.clear();
+        cin.ignore(10000, '\n');
+
+        endScreen();
+    }
+
+    if (endScreenChoice == ADD) {
+        add();
+    }
+
+    if (endScreenChoice == SUBTRACT) {
+        subtract();
+    }
+
+    if (endScreenChoice == MULTIPLY) {
+        multiply();
+    }
+
+    if (endScreenChoice == DIVIDE) {
+        divide();
+    }
+
+    if (endScreenChoice == END) {
+        cout << "Program ended..." << endl;
+    }
 }
